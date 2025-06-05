cb7a6004138da101bcaa887040321e61b6c5d95a
张帅威\swzha <swzhang3@outlook.com>
2025-06-05 10:40:11 +0800

    1、修改接口地址

--- module-network/src/main/java/club/ccit/network/news/model/NewsListResultModel.java
+++ module-network/src/main/java/club/ccit/network/news/model/NewsListResultModel.java
@@ -7,6 +7,15 @@ import java.util.List;
 // 新闻列表请求返回数据
 public class NewsListResultModel {
     private List<NewsListResultData> result;
+    private int total;
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
 
     public List<NewsListResultData> getResult() {
         return result == null ? new ArrayList<>() : result;
